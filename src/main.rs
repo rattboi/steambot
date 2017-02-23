58f8b2a8105d6c0028ecf029172a632f71a95e1e
Bradon Kanyid <bradon@kanyid.org>
2017-02-23 11:29:14 -0800

    Created SteamCommand struct/impl

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,44 @@ extern crate irc;
 
 use irc::client::prelude::*;
 
-fn handle_command(server: &IrcServer, target: &String, args: Vec<&str>) {
-  for argument in args {
-    server.send_privmsg(target, argument).unwrap();
+struct SteamCommand<'a> {
+  server: &'a IrcServer,
+  target: &'a String,
+  args: Vec<&'a str>
+}
+
+impl<'a> SteamCommand<'a> {
+  fn new(server: &'a IrcServer, target: &'a String, msg: &'a String) -> Result<SteamCommand<'a>, &'static str> {
+    let args:Vec<&str> = msg.split_at(1).1.split(' ').collect();
+    let command = args[0];
+
+    match command.to_lowercase().as_ref() {
+      "reg" | 
+      "price" | 
+      "now" |
+      "game" => { 
+        Ok(SteamCommand {
+          server: server,
+          target: target,
+          args: args
+        })
+      },
+      _ => Err("No such command")
+    }
+  }
+}
+
+fn handle_command(command: SteamCommand) {
+  for argument in command.args {
+    command.server.send_privmsg(command.target, argument).unwrap();
   }
 }
 
 fn handle_privmsg(server: &IrcServer, target: &String, msg: &String) {
-  let args:Vec<&str> = msg.split_at(1).1.split(' ').collect();
-  let command = args[0];
-
-  match command.to_lowercase().as_ref() {
-    "reg" | 
-    "price" | 
-    "now" |
-    "game" => { 
-      handle_command(server, target, args);
-    },
-    _ => ()
+  let command = SteamCommand::new(server, target, msg);
+  match command {
+    Ok(command) => handle_command(command),
+    Err(err_msg) => print!("{}", err_msg)
   }
 }
 
